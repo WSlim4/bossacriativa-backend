ce40ef86433ae1820fd3ec3d348afef09d05cb45
Wesley Lima <wesleyjoga500@gmail.com>
2020-06-16 18:48:55 -0300

    deploy

--- config/database.js
+++ config/database.js
@@ -78,8 +78,8 @@ module.exports = {
      host: Env.get('DB_HOST', DATABASE_URL.hostname),
      port: Env.get('DB_PORT', DATABASE_URL.port),
      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+     password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+     database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
    },
    debug: Env.get('DB_DEBUG', false)
   }
