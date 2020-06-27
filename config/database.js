b3db15935f700a40b2f8af90eeed1d81ce148d34
Wesley Lima <wesleyjoga500@gmail.com>
2020-06-26 22:55:34 -0300

    deploy

--- config/database.js
+++ config/database.js
@@ -1,9 +1,6 @@
 'use strict'
 
-/** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
-
-/** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 const Url = require('url-parse')
 const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
@@ -72,15 +69,15 @@ module.exports = {
   | npm i --save pg
   |
   */
-  pg: {
-    client: 'pg',
-    connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.hostname),
-      port: Env.get('DB_PORT', DATABASE_URL.port),
-      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
-    },
-    debug: Env.get('DB_DEBUG', false)
+ pg: {
+  client: 'pg',
+  connection: {
+    host: Env.get('DB_HOST', DATABASE_URL.hostname),
+    port: Env.get('DB_PORT', DATABASE_URL.port),
+    user: Env.get('DB_USER', DATABASE_URL.username),
+    password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+    database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+  },
+  debug: Env.get('DB_DEBUG', false)
   }
 }
